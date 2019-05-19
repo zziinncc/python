38a1466e125e35c4e613cd1e2f65e5f16c31c04f
singal <singalzinc@gmail.com>
2019-05-19 12:05:10 +0800

    add test file

--- test.py
+++ test.py
@@ -1,11 +1,25 @@
-import numpy as np
+from typing import List
+'''
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numsMap = {}
+        for index,num in enumerate(nums):
+            tmpDiff = target-num
+            if tmpDiff in numsMap.keys():
+                return [numsMap[tmpDiff],index]
+            numsMap[num] = index
+        return None 
+'''
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for j, x in enumerate(nums):
+            temp = target - x
+            if d.get(temp) != None:
+            #if temp in d.keys():
+                return [d[temp], j]
+            d[x] = j
+        return None
+a = Solution()
 
-import matplotlib.pyplot as plt
-
-x = np.linspace(0, 1, 1000)
-
-y = np.sin(2 * np.pi * 30 * x)
-
-plt.plot(x, y)
-
-plt.show()
+print(a.twoSum([3,3], 6))
